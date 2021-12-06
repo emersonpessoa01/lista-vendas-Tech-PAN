d7bc4622342c2892f9e26c153a5b23ccb8283859
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2021-12-05 21:17:57 -0300

    updated SecurityConfig

---
+++ lista-vendas-TECH-PAN/src/main/java/com/emersonpessoa/listavendasTECHPAN/ListaVendasTechPanApplication.java
@@ -0,0 +1,13 @@
+package com.emersonpessoa.listavendasTECHPAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaVendasTechPanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaVendasTechPanApplication.class, args);
+	}
+
+}
