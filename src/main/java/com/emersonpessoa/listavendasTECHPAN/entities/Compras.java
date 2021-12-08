4b86a119b5f09f79b9a91736a52c6a4f83cbd5d2
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2021-12-08 17:53:23 -0300

    added entity compras

---
+++ src/main/java/com/emersonpessoa/listavendasTECHPAN/entities/Compras.java
@@ -0,0 +1,56 @@
+package com.emersonpessoa.listavendasTECHPAN.entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_compras") // create table tb_cliente
+public class Compras {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // incremento
+	private int id;
+	private Double totalCompra;
+	private LocalDate dataCompra;
+	
+	@ManyToOne//cada compra pode ser feito apenas por um cliente
+	@JoinColumn(name="cliente_id")//chave estrangeira
+	// criar relacionamento de cliente com compras
+	private Cliente cliente;
+	
+	public Compras(int id, Double totalCompra, LocalDate dataCompra) {
+		this.id = id;
+		this.totalCompra = totalCompra;
+		this.dataCompra = dataCompra;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Double getTotalCompra() {
+		return totalCompra;
+	}
+	public void setTotalCompra(Double totalCompra) {
+		this.totalCompra = totalCompra;
+	}
+	public LocalDate getDataCompra() {
+		return dataCompra;
+	}
+	public void setDataCompra(LocalDate dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+
+	
+}
--- src/main/java/com/emersonpessoa/listavendasTECHPAN/services/ClienteService.java
+++ src/main/java/com/emersonpessoa/listavendasTECHPAN/services/ClienteService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.el.parser.AstListData;
 import org.springframework.stereotype.Service;
 
 import com.emersonpessoa.listavendasTECHPAN.entities.Cliente;
