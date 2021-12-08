a5fabfeab3fcd788e622ae20fd7425d4d14e0f14
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2021-12-08 15:36:59 -0300

    added ResponseEntity in ClienteController

--- src/main/java/com/emersonpessoa/listavendasTECHPAN/controllers/ClienteController.java
+++ src/main/java/com/emersonpessoa/listavendasTECHPAN/controllers/ClienteController.java
@@ -1,15 +1,27 @@
 package com.emersonpessoa.listavendasTECHPAN.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emersonpessoa.listavendasTECHPAN.entities.Cliente;
+import com.emersonpessoa.listavendasTECHPAN.services.ClienteService;
+
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+	@Autowired
+	private ClienteService clienteService;
+	
 	@GetMapping
-	public String teste() {
-		return "Olá, Emerson Pessoa";
+	public ResponseEntity<List<Cliente>>findAll(){
+		clienteService.saveCliente();
+		List<Cliente> lista = clienteService.findAll();
+		return ResponseEntity.ok(lista);
 	}
 
 }
